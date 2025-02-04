9ffb5d27981824184fb171364b2ad2465abb086a
Gleb Pavliuk <33667408+tastyCheese@users.noreply.github.com>
2025-02-04 11:22:42 +0100

    Add hoverable labels to points (#18)
    * Added labels to points on mouse hover
    
    * Added label to the round location
    
    * Cleanup
    
    * Cleanup
    
    * Update src/helpers/render.ts
    
    Fix for missing argument
    
    ---------
    
    Co-authored-by: Gleb Pavliuk <gleb@zagravatrading.com>
    Co-authored-by: Nathan Edwards <npfedwards@gmail.com>

--- src/helpers/render.ts
+++ src/helpers/render.ts
@@ -1,6 +1,7 @@
 import type {GeoPath, GeoSphere} from "d3";
 import type {MultiLineString, FeatureCollection, MultiPoint} from "geojson";
 import type {Selection} from "d3-selection";
+import Point from "@/classes/point.ts";
 
 const render = (
   event: Selection<HTMLCanvasElement, unknown, null, undefined> | null,
@@ -12,11 +13,13 @@ const render = (
   land: FeatureCollection,
   mesh: FeatureCollection,
   sphere: GeoSphere,
-  points: number[][],
+  points: Point[],
   colours: string[],
   lat: number,
   lng: number,
   found: number | undefined,
+  mouseX: number,
+  mouseY: number,
   selectedLat: number,
   selectedLng: number
   ): void => {
@@ -64,7 +67,7 @@ const render = (
   }
 
   context.beginPath();
-  path({type: "MultiPoint", coordinates: points} as MultiPoint);
+  path({type: "MultiPoint", coordinates: points.map((point) => [point.longitude, point.latitude])} as MultiPoint);
   context.fillStyle = "#f00";
   context.fill();
 
@@ -80,6 +83,55 @@ const render = (
     path({type: "Point", coordinates: [selectedLng, selectedLat]});
     context.fillStyle = "#37ff00";
     context.fill();
+  }
+
+  if (mouseX && mouseY) {
+    const hoverRadius = 4;
+    let closestPoint = null;
+    let closestRendered = null;
+    const closestDistance = hoverRadius;
+    points.forEach(point => {
+      const renderedPoint = path.centroid({type: "Point", coordinates: [point.longitude, point.latitude]});
+      const distance = Math.sqrt(Math.pow(renderedPoint[0] - mouseX, 2) + Math.pow(renderedPoint[1] - mouseY, 2));
+      if (distance < closestDistance) {
+        closestPoint = point;
+        closestRendered = {
+          x: renderedPoint[0],
+          y: renderedPoint[1]
+        };
+      }
+    });
+    context.fillStyle = "#f00";
+
+
+    if (lng !== 0 && lat !== 0) {
+      const renderedRound = path.centroid({type: "Point", coordinates: [lng, lat]});
+      const roundDistance = Math.sqrt(Math.pow(renderedRound[0] - mouseX, 2) + Math.pow(renderedRound[1] - mouseY, 2));
+      if (roundDistance < closestDistance) {
+        closestPoint = new Point(lng, lat, 'Current round', '');
+        closestRendered = {
+          x: renderedRound[0],
+          y: renderedRound[1]
+        };
+        context.fillStyle = "#000dff";
+      }
+    }
+    if (closestPoint && closestRendered) {
+      const oldRadius = path.pointRadius();
+      path.pointRadius(hoverRadius);
+
+      context.beginPath();
+      path({type: "Point", coordinates: [closestPoint.longitude, closestPoint.latitude]});
+      context.fill();
+
+      // Tooltip
+      context.fillStyle = "#fff";
+      context.font = "bold 14px sans";
+      context.textAlign = "center";
+      context.fillText(closestPoint.label, closestRendered.x, closestRendered.y - 15);
+
+      path.pointRadius(oldRadius);
+    }
   }
 };
 
